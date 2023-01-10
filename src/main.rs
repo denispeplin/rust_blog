aed05749f321737fe42a6c2800420544d8c1dd15
denispeplin <denis.peplin@gmail.com>
2023-01-10 16:17:41 +0300

    Fix `cargo clippy` warning (variable can be used directly)

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use rocket::response::content;
 // http://localhost:8000/post/your-post-name
 #[get("/post/<post_name>")]
 fn post(post_name: String) -> content::Html<String> {
-    let file_path = format!("posts/{}.md", post_name);
+    let file_path = format!("posts/{post_name}.md");
     let markdown_text = std::fs::read_to_string(file_path).unwrap();
     let parser = pulldown_cmark::Parser::new(&markdown_text);
     let mut html_text = String::new();
